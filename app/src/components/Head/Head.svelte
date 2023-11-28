b4c025daa66e61d673aa92e232fc541a762c85d4
Stefan Kowal <kowalmax.s@gmail.com>
2023-11-28 12:26:35 -0800

    svelte version change, og dat fix

--- app/src/components/Head/Head.svelte
+++ app/src/components/Head/Head.svelte
@@ -1,10 +1,10 @@
 <script>
   export let data;
-  const isBlogPost = data.slug !== undefined;
-	const image = isBlogPost ? data.mainImage.image :data.ogImage;
+  const isBlogPost = data._type === 'post';
+	const image = isBlogPost ? data.mainImage.image : data.ogImage;
 	const description = isBlogPost ? data.description : data.ogDescription;
 	const title = isBlogPost ? data.title : data.ogTitle 
-	const url = isBlogPost ? `https://www.stefankowal.com/blog/${data.slug.current}` : (data._type === 'home' ? '/' : data._type);
+	const url = isBlogPost ? `https://www.stefankowal.com/blog/${data.slug.current}` : (data._type === 'home' ? 'https://www.stefankowal.com' : `https://www.stefankowal.com/${data._type}`);
 </script>
 <svelte:head>
 	<meta property="og:url" content={url} />
