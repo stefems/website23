01ef29112eab6500c413dbeb32b75fec8a036a8e
Stefan Kowal <kowalmax.s@gmail.com>
2023-11-27 16:13:17 -0800

    extra og meta

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 
 <svelte:head>
 	{#if data && data.title && data.description && data.mainImage}
+		<meta property="og:url" content={`https://www.stefankowal.com/blog/${data.slug}`} />
+		<meta property="twitter:url" content={`https://www.stefankowal.com/blog/${data.slug}`} />
+		<meta name="twitter:image" content={data.mainImage.image} />
 		<meta property="og:title" content={data.title} />
 		<meta property="og:description" content={data.description} />
 		<meta property="og:image" content={data.mainImage.image} />
